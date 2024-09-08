b71e0b90f118f932178bc738f01ca336b8f7f4f7
Steddy <qwesteddyoff@gmail.com>
2024-09-08 11:34:01 +0300

    add axios + RandomPokemon

--- src/components/Random/RandomPokemon.jsx
+++ src/components/Random/RandomPokemon.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import cl from "./RandomPokemon.module.css";
 
+// https://pokeapi.co/api/v2/pokemon?offset=5&limit=1
+
 const RandomPokemon = () => {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const getRandomPokemon = async () => {
+      try {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
+        );
+        setPokemon(response.data);
+      } catch (error) {
+        console.log(`Ошибка: ${error}`);
+      }
+    };
+    getRandomPokemon();
+  }, []);
+  console.log(pokemon);
+
   return (
     <div className={cl.random}>
-      <div className={cl.random__wrapper}></div>
+      {pokemon ? <p>{pokemon.name}</p> : <div>Loading...</div>}
+      {pokemon ? (
+        <div className={cl.random__wrapper}>
+          <img src={pokemon.sprites.front_default} />
+        </div>
+      ) : (
+        <div>Loading</div>
+      )}
+
       <p>Рандомный покемон</p>
     </div>
   );
