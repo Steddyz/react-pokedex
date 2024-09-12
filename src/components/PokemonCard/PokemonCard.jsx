076b07a233d34675a282dff8913dd741bfd24ef1
Steddy <qwesteddyoff@gmail.com>
2024-09-12 11:51:49 +0300

    commit

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -3,26 +3,36 @@ import axios from "axios";
 
 import cl from "./PokemonCard.module.css";
 
-// https://pokeapi.co/api/v2/pokemon?offset=5&limit=1
-// https://pokeapi.co/api/v2/pokemon?limit=20
-
 const PokemonCard = () => {
   const [pokemons, setPokemons] = useState([]);
+
+  let page = 0;
 
   useEffect(() => {
     const allPokemons = async () => {
       const response = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=20"
+        `https://pokeapi.co/api/v2/pokemon?offset=${page}?limit=20`
       );
-      console.log(response.data);
-      setPokemons(response.data.results);
+      const pokemonData = await Promise.all(
+        response.data.results.map(async (pok) => {
+          const pokResponse = await axios.get(pok.url);
+          return {
+            name: pokResponse.data.name,
+            img: pokResponse.data.sprites.front_default,
+          };
+        })
+      );
+      setPokemons(pokemonData);
     };
     allPokemons();
   }, []);
   return (
     <div>
       {pokemons.map((pok) => (
-        <div>{pok.name}</div>
+        <div key={pok.id}>
+          <img src={pok.img} alt={pok.name} />
+          <div>{pok.name}</div>
+        </div>
       ))}
     </div>
   );
